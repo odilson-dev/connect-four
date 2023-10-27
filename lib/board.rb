06e9a101b92b5cecdf6a7553226aba4266a4eb09
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-10-27 14:13:44 -0400

    Implement #display_board method

--- lib/board.rb
+++ lib/board.rb
@@ -6,5 +6,16 @@ class Board
         @board = Array.new(rows) { Array.new(columns, ' ') }
       end
 
- 
+      def display_board
+        puts "  1   2   3   4   5   6   7"
+        puts "+---+---+---+---+---+---+---+"
+        @rows.times do |row|
+            row_display = "|"
+            @columns.times do |col|
+                row_display += " #{@board[row][col]} |"
+            end
+            puts row_display
+        puts "+---+---+---+---+---+---+---+"
+        end
+      end
 end
